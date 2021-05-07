559cf173af51e2b69e92c9f7fb5366bf2ed888ef
Cedric Stark <cedric.stark@outlook.com>
2021-05-07 16:33:29 +0200

    2021-05-07: Massively improve memory efficiency

--- 2021/May/Week_1/2021_05_07_string_deletion.py
+++ 2021/May/Week_1/2021_05_07_string_deletion.py
@@ -2,7 +2,9 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         len1 = len(word1)
         len2 = len(word2)
-        editDistances = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+        # editDistances = [[0 for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+        # we actually only need the current and the previous line => less memory
+        editDistances = [[0 for _ in range(len2 + 1)] for _ in range(2)]
 
         for i in range(len1 + 1):
             for j in range(len2 + 1):
@@ -11,10 +13,10 @@ class Solution:
                 elif not j:
                     d = i
                 elif word1[i-1] == word2[j-1]:
-                    d = editDistances[i-1][j-1]
+                    d = editDistances[(i-1) % 2][j-1]
                 else:
-                    d = 1 + min(editDistances[i-1][j], editDistances[i][j-1])
-                editDistances[i][j] = d
+                    d = 1 + min(editDistances[(i-1) % 2][j], editDistances[i % 2][j-1])
+                editDistances[i % 2][j] = d
         return d
         
 
