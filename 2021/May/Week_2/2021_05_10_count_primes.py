97b0953ad4df6020ad5fd7629c3dd832b281782e
Cedric Stark <cedric.stark@outlook.com>
2021-05-10 16:41:25 +0200

    2021-05-10: reduce time complexity by improving boundary design

--- 2021/May/Week_2/2021_05_10_count_primes.py
+++ 2021/May/Week_2/2021_05_10_count_primes.py
@@ -6,8 +6,8 @@ class Solution:
         numbers[0] = 0
         numbers[1] = 0
         f = 2
-        while f < n:
-            for i in range(2, n // f + 1):
+        while f < n**0.5:
+            for i in range(f, n // f + 1):
                 numbers[i*f] = 0
             f += 1
             while numbers[f] == 0 and f < n:
@@ -33,3 +33,4 @@ for i, (n, p) in enumerate(testcases):
     guess = Solution().countPrimes(n)
     print('Case #{} should be {}, is {}.'.format(i+1, p, guess))
     assert guess == p
+print('Tests passed!')
