f10be18a14c0e87e4ee65a58bc5f9f251d78217a
Cedric Stark <cedric.stark@outlook.com>
2021-06-23 10:37:25 +0200

    2021-06-23: solve with generic reverse method and detaching the part to be reversed

---
+++ 2021/06_June/Week_4/2021_06_23_reverse_linked_list_2.py
@@ -0,0 +1,79 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # O(n) time complexity: passing through reversed part twice, but still linear
+    # O(1) space complexity: no extra nodes created
+    def reverseBetween2Pass(self, head: ListNode, left: int, right: int) -> ListNode:
+        # generic reverse
+        def reverse(head):
+            curr = head
+            next = curr.next
+            curr.next = None
+            while next is not None:
+                prev = curr
+                curr = next
+                next = next.next
+                curr.next = prev
+            return curr, head
+
+        firstNode, lastNode = head, head
+        left_i, right_i = left, right
+        prev, next = None, lastNode.next
+        # find start of part to reverse
+        while left_i > 1:
+            prev = firstNode
+            firstNode = firstNode.next
+            lastNode = lastNode.next
+            next = lastNode.next
+            left_i -= 1
+            right_i -= 1
+        # end of part to reverse
+        while right_i > 1:
+            lastNode = lastNode.next
+            next = lastNode.next
+            right_i -= 1
+
+        # detach list to be reversed
+        lastNode.next = None
+
+        # stitch list back together
+        reversed = reverse(firstNode)
+        if left > 1: 
+            prev.next = reversed[0]
+        else: 
+            head = reversed[0]
+        reversed[1].next = next
+
+        return head
+
+
+
+
+
+testcases = [
+    ([1,2,3,4,5], 2, 4, [1,4,3,2,5]),
+    ([5], 1, 1, [5]),
+    ([3,5], 1, 1, [3,5])
+]
+
+for i, (input, left, right, target) in enumerate(testcases):
+    head = ListNode(input[0])
+    node = head
+    for entry in input[1:]:
+        next = ListNode(entry)
+        node.next = next
+        node = next
+    output = []
+    reversedList = Solution().reverseBetween(head, left, right)
+    while reversedList is not None:
+        output.append(reversedList.val)
+        reversedList = reversedList.next
+    print('Case #{}: should be\n{}, is\n{}'.format(i+1, target, output))
+    assert target == output
+print('All test cases passed!')
+
