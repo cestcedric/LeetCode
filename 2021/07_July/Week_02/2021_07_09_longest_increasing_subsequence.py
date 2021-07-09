66c7cc7e23585bb5eaedb047cea2c668ba0f0b88
Cedric Stark <cedric.stark@outlook.com>
2021-07-09 11:01:48 +0200

    2021-07-09: solve with binary search

--- 2021/07_July/Week_02/2021_07_09_longest_increasing_subsequence.py
+++ 2021/07_July/Week_02/2021_07_09_longest_increasing_subsequence.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 class Solution:
     # O(n^2) time complexity: look up all previous entries for every entry
     # O(n) space complexity: additional dp array of size n
@@ -11,15 +13,33 @@ class Solution:
         return max(dp)
 
 
+    # O(n * log(n)) time complexity: one pass through nums, binary search in sorted seq
+    # O(n) space complexity: seq with at most n elements
+    def lengthOfLIS2(self, nums: list) -> int:
+        seq, s = [], 0
+
+        for n in nums:
+            index = bisect_left(seq, n)
+            if index == s: 
+                seq.append(n)
+                s += 1
+            elif seq[index] > n:
+                seq[index] = n
+            elif seq[index] < n:
+                seq[index + 1] = n
+
+        return s
+
 testcasses = [
     ([10,9,2,5,3,7,101,18], 4),
     ([0,1,0,3,2,3], 4),
     ([7,7,7,7,7,7,7], 1),
-    ([1,3,6,7,9,4,10,5,6], 6)
+    ([1,3,6,7,9,4,10,5,6], 6),
+    ([1,2,3,4,0,3], 4)
 ]
 
 for i, (input, target) in enumerate(testcasses):
-    output = Solution().lengthOfLIS(input)
+    output = Solution().lengthOfLIS2(input)
     print('Case #{}: should be {}, is {}'.format(i + 1, target, output))
     assert target == output
 print('All test cases passed!')
