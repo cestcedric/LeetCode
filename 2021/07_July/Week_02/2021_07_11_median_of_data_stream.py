f8584a4c189df7c4b1d0f2e5f7d2f72bc1bb0143
Cedric Stark <cedric.stark@outlook.com>
2021-07-11 18:22:02 +0200

    2021-07-11: solve with heaps

--- 2021/07_July/Week_02/2021_07_11_median_of_data_stream.py
+++ 2021/07_July/Week_02/2021_07_11_median_of_data_stream.py
@@ -1,4 +1,5 @@
 from sortedcontainers import SortedList
+import heapq
 
 class MedianFinderSorted:
 
@@ -21,7 +22,32 @@ class MedianFinderSorted:
             return (self.data[mid - 1] + self.data[mid]) / 2
 
 
-m = MedianFinderSorted()
+class MedianFinderHeap:
+
+    # O(n) space complexity
+    def __init__(self):
+        self.minHeap = []
+        self.maxHeap = []
+        self.minSize = 0
+        self.maxSize = 0
+
+    # O(log(n / 2)) time complexity per insertion, O(n log(n / 2)) total
+    def addNum(self, num: int) -> None:
+        if self.minSize == self.maxSize:
+            heapq.heappush(self.minHeap, -heapq.heappushpop(self.maxHeap, num))
+            self.minSize += 1
+        else:
+            heapq.heappush(self.maxHeap, -heapq.heappushpop(self.minHeap, -num))
+            self.maxSize += 1
+
+    # O(1) time complexity
+    def findMedian(self) -> float:
+        if self.maxSize != self.minSize: return -self.minHeap[0]
+        else: return (self.maxHeap[0] - self.minHeap[0]) / 2
+        
+
+
+m = MedianFinderHeap()
 m.addNum(1)
 m.addNum(2)
 assert m.findMedian() == 1.5
