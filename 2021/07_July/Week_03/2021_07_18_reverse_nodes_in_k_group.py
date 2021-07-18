4174268dec0ec93ff08a5ed11f36f550e31dbd14
Cedric Stark <cedric.stark@outlook.com>
2021-07-18 20:00:52 +0200

    2021-07-17: split list into groups of k nodes and completely reverse sublists + stitch together

---
+++ 2021/07_July/Week_03/2021_07_18_reverse_nodes_in_k_group.py
@@ -0,0 +1,80 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # O(n) time complexity: 
+    # -> loop over each node once
+    # -> n // k times: reverse list in O(k)
+    #  => O(n // k * k) = O(n) total reversing time complexity
+    # O(n) space complexity: only dependent on reverseList implementation
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        if head.next is None: return head
+        # if k == 1: return head # shortcut
+        sentinel = ListNode()
+        sentinel.next = head
+        pred, first, node, i = sentinel, head, head, 1
+
+        while node is not None:
+            if i < k:
+                node = node.next
+                i += 1
+            else:
+                succ = node.next
+                node.next = None
+                pred.next, pred = self.reverseList(first)
+                first = pred.next = node = succ
+                i = 1
+
+        return sentinel.next
+
+    # O(n) time complexity: looks at every node twice
+    # O(n) space complexity: queue stores all nodes at one point
+    def reverseList(self, node):
+        if node is None: return None, None
+        queue = []
+        while node is not None:
+            queue.append(node)
+            node = node.next
+            
+        head = queue.pop()
+        node = head
+        while queue != []:
+            node.next = queue.pop()
+            node = node.next
+        node.next = None
+
+        return head, node
+
+
+def linkedListToArray(node):
+    output = []
+    while node is not None:
+        output.append(node.val)
+        node = node.next
+    return output
+
+def arrayToLinkedList(arr):
+    head = ListNode(arr[0])
+    node = head
+    for n in arr[1:]:
+        node.next = ListNode(n)
+        node = node.next
+    return head
+
+testcases = [
+    ([1, 2, 3, 4, 5], 3, [3, 2, 1, 4, 5]),
+    ([1, 2, 3, 4, 5], 1, [1, 2, 3, 4, 5]),
+    ([1], 1, [1])
+]
+
+for i, (nodes, k, target) in enumerate(testcases):
+    head = arrayToLinkedList(nodes)
+    output = Solution().reverseKGroup(head, k)
+    outputArr = linkedListToArray(output)
+    print('Case #{}: should be\n{}, is\n{}'.format(i + 1, target, outputArr))
+    assert target == outputArr
+print('All test cases passed!')
