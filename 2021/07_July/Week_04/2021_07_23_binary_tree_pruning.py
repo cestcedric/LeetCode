08e400da8cbc5fab333898006db2c45cefcaeb49
Cedric Stark <cedric.stark@outlook.com>
2021-07-23 16:36:10 +0200

    2021-07-23: solve with recursion

---
+++ 2021/07_July/Week_04/2021_07_23_binary_tree_pruning.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # O(n) time complexity: look at each node once
+    # O(h) space complexity: call stack depth = tree height, depends on how balanced
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if root is None: return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+
+        if root.left is None and root.right is None and root.val == 0:
+            return None
+        
+        return root
