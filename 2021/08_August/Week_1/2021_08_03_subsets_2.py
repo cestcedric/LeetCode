e18ac560a9fba4d93c387006d8fe393b5712bf0b
Cedric Stark <cedric.stark@outlook.com>
2021-08-03 11:19:11 +0200

    2021-08-03: add solution not based on sets

--- 2021/08_August/Week_1/2021_08_03_subsets_2.py
+++ 2021/08_August/Week_1/2021_08_03_subsets_2.py
@@ -1,8 +1,12 @@
+from collections import Counter
+
+
 class Solution:
     # O(2^n) time complexity: number of subsets at most 2^n
     # O(n) space complexity: tmp with at most n elements, subsets = output not counted
-    def subsetsWithDup(self, nums: list) -> list:
+    def subsetsWithDupSet(self, nums: list) -> list:
         subsets = set()
+        nums.sort()
 
         for i in range(2 ** len(nums)):
             tmp = []
@@ -13,6 +17,31 @@ class Solution:
 
         return subsets
 
+
+    # O(n^2) time complexity: number of subsets
+    # O(1) space complexity: all additionally used space used for output
+    def subsetsWithDup(self, nums: list) -> list:
+        counter = Counter(nums)
+        nonzero = [(n, counter[n]) for n in counter]
+
+        output = [[]]
+
+        while nonzero != []:
+            n, c = nonzero.pop()
+            frontier = []
+
+            for i in range(1, c + 1):
+                for o in output:
+                    frontier.append(o + [n] * i)
+
+            output.extend(frontier)
+
+        return output
+
+
+
+
+            
 
 testcases = [
     ([1,2,2], [[],[1],[1,2],[1,2,2],[2],[2,2]]),
