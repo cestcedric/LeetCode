0c106b07066594a4bfe15235eb2737385cb7957f
Cedric Stark <cedric.stark@outlook.com>
2021-09-04 11:51:22 +0200

    2021-09-04: solve with DFS and DP

---
+++ 2021/09_September/Week_01/2021_09_04_sum_of_distances_in_tree.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Solution:
+    # O(n) time
+    # O(n) space
+    def sumOfDistancesInTree(self, n: int, edges: list) -> list:
+        graph = defaultdict(set)
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * n
+        ans = [0] * n
+
+        def dfs(node, parent):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node, parent):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + n - count[child]
+                    dfs2(child, node)
+
+        dfs(0, None)
+        dfs2(0, None)
+        return ans
