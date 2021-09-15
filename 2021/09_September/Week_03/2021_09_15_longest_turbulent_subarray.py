a2320926c357ba71f9de57b3f206658b4eba3a54
Cedric Stark <cedric.stark@outlook.com>
2021-09-15 10:56:21 +0200

    2021-09-15: improve space complexity with sliding window approach

--- 2021/09_September/Week_03/2021_09_15_longest_turbulent_subarray.py
+++ 2021/09_September/Week_03/2021_09_15_longest_turbulent_subarray.py
@@ -1,7 +1,7 @@
 class Solution:
     # O(n) time
     # O(n) space
-    def maxTurbulenceSize(self, arr: list) -> int:
+    def maxTurbulenceSizeDP(self, arr: list) -> int:
         n = len(arr)
         if n < 2: return n
 
@@ -21,6 +21,31 @@ class Solution:
             sign = s
 
         return max(dp)
+
+
+    # O(n) time
+    # O(1) space
+    def maxTurbulenceSize(self, arr: list) -> int:
+        n = len(arr)
+        if n < 2: return n
+
+        def diffSign(x, y):
+            if x == y: return 0
+            if x < y: return -1
+            return 1
+
+        maxSubarray = 0
+        start = 0
+
+        sign = None
+        for i in range(1, n):
+            s = diffSign(arr[i - 1], arr[i])
+            if s == 0: start = i
+            elif s == sign: start = i - 1
+            maxSubarray = max(maxSubarray, i - start + 1)
+            sign = s
+
+        return maxSubarray
 
 
 
