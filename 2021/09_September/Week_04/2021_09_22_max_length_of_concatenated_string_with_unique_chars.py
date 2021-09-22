92a1b96f43962f11c5ee63e8e3a43cda767035ba
Cedric Stark <cedric.stark@outlook.com>
2021-09-22 14:11:40 +0200

    2021-09-22: add backtracking solution

--- 2021/09_September/Week_04/2021_09_22_max_length_of_concatenated_string_with_unique_chars.py
+++ 2021/09_September/Week_04/2021_09_22_max_length_of_concatenated_string_with_unique_chars.py
@@ -14,7 +14,7 @@ class Solution:
     # O(2^n) time: we always try every combination
     # O(n) space: temporary candidate list at most contains all input strings
     # obviously bruteforce solution, but works given the small input size
-    def maxLength(self, arr: list) -> int:
+    def maxLengthBruteforce(self, arr: list) -> int:
         n = len(arr)
         if n == 1: return len(arr[0])
 
@@ -32,11 +32,49 @@ class Solution:
         return maxLength
 
 
+    # O(2^n) time: on average a lot quicker than bruteforce bc. early pruning
+    # O(n) space: call stack depth, at most combining all words
+    def maxLength(self, arr: list) -> int:
+
+        # O(1) time
+        def containsDuplicates(s: str) -> bool:
+            chars = [0] * 26
+            for c in s:
+                if chars[ord(c) - ord('a')] == 1: return True
+                chars[ord(c) - ord('a')] = 1
+            return False
+
+        # O(n) time
+        def fingerprint(s: str) -> list:
+            fp = ['0'] * 26
+            for c in s: fp[ord(c) - ord('a')] = '1'
+            return int(''.join(fp), 2) 
+        
+        def backtrack(prefix: str, position: int) -> int:
+            nonlocal arr
+
+            prefixFP = fingerprint(prefix)
+            maxLength = len(prefix)
+
+            for i in range(position + 1, len(arr)):
+                if prefixFP & fingerprint(arr[i]) == 0:
+                    maxLength = max(maxLength, backtrack(prefix + arr[i], i))
+                    if maxLength == 26: break
+
+            return maxLength
+
+        arr = [s for s in arr if not containsDuplicates(s)]
+        if len(arr) == 0: return 0
+        return max(backtrack(s, i) for i, s in enumerate(arr))
+
+
+
 testcases = [
     (['un','iq','ue'], 4),
     (['cha','r','act','ers'], 6),
     (['abcdefghijklmnopqrstuvwxyz'], 26),
-    (['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'], 16)
+    (['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'], 16),
+    (["yy","bkhwmpbiisbldzknpm"], 0)
 ]
 
 
